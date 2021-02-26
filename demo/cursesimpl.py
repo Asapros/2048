4da6034c613c7306957d28bf71b9007a2c44b97c
Asapros <asapros@wp.pl>
2021-02-26 09:18:49 +0100

    Documented code

--- demo/cursesimpl.py
+++ demo/cursesimpl.py
@@ -1,6 +1,5 @@
 # pip install windows-curses
-import sys
-sys.path.insert(0,'..')
+import sys; sys.path.insert(0,'..') # Haha import hacking goes brrrrrrr
 from main import Board, Directions
 import curses
 
@@ -29,7 +28,7 @@ def play(screen):
         boardstate = board.visualize(border=SHOW_BORDER, fixsize=FIX_CELL_SIZE)
         
         try: screen.addstr(boardstate)
-        except:
+        except: # One reason of any error happening here is that curses can't move cursor further
             print("Your screen is too small!")
             return
         
@@ -50,12 +49,12 @@ def play(screen):
         
 if __name__ == "__main__":
     try: curses.initscr()
-    except AttributeError: input("You can't open it from IDE!")
+    except AttributeError: input("You can't open it from IDE!") # AttributeError is raised when opening in editor, I don't really know why, either way there is no point of playing in IDE cause cursor movement is not working there anyway.
     curses.noecho()
     curses.cbreak()
     curses.curs_set(0)
     
-    board = Board(width=BOARD_WIDTH, height=BOARD_HEIGHT)
+    board = Board(width=BOARD_WIDTH, height=BOARD_HEIGHT) # Initalizes the board, see more info in main.py
     curses.wrapper(play)
     
     curses.endwin()
--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+"""
+This is the main logic of 2048 game.
+How do I document functions:
+
+[-] Parametrs:
+    [-] Optional
+    :optional([default value]) param [name] [type]: -> [what the parametr does]
+    
+    [-] Required
+    :param [name] [type]: -> [what the parametr does]
+
+    (If the type is from enum I use 'enum [name of enum]' instead of '[type]')
+    
+[-] Return values:
+    :return [type]: -> [when it returns what]
+
+[-] Error raising:
+    :raise [name of exception]: -> [when the exception is raised] 
+"""
+
 from enum   import Enum
 from copy   import copy
 from math   import log
@@ -11,19 +31,41 @@ class Directions(Enum):
     LEFT = 3
 
 class Board:
-    def __init__(self, width=10, height=10, fill=True):
+    """This is a grid with numbers"""
+    def __init__(self, width: int = 10, height: int = 10, fill: bool = True):
+        """
+Initalization
+:optional(10)   param width  int:  -> set width of the board
+:optional(10)   param height int:  -> set height of the board
+:optional(True) param fill   bool: -> fill the board on initalization?
+        """
         self.size = (width, height)
         self._array = {}
         if fill: self.random_fill(percent=25)
     # [ ------------------------------------------------------------- ] #
     @property
-    def width(self): return self.size[0]
+    def width(self):
+        """
+Property for width
+:return int: -> width
+        """
+        return self.size[0]
     @property
-    def height(self): return self.size[1]
+    def height(self):
+        """
+Property for height
+:return int: -> height
+        """
+        return self.size[1]
 
-    CHARSET = [" ", ".", "*", ":", "&", "%", "$", "@", "#"]
+    CHARSET = [" ", ".", "*", ":", "&", "%", "$", "@", "#"] # Character set for _number_to_char
     @classmethod
     def _number_to_char(cls, number):
+        """
+Converts number from the board to character in specific intensity
+:param number int: -> Number you want to convert
+:return str: -> The character
+        """
         if number <= 0: return cls.CHARSET[0]
         
         index = int(log(number, 2))+1
@@ -35,6 +77,11 @@ class Board:
         
     # [ ------------------------------------------------------------- ] #
     def random_fill(self, number=1, percent=10):
+        """
+Fills board in random pattern
+:optional(1)  param number  int: -> Number you want to spread
+:optional(10) param percent int: -> Chance in 100 that field will be filled
+        """
         for y in range(0, self.height):
             for x in range(0, self.width):
                 if randint(0,100) <= percent:
@@ -42,31 +89,68 @@ class Board:
                         self.place_number(number,x,y)
     
     def in_bounds(self, x, y):
+        """
+Checks if coordinates are in bounds
+:param x int: -> x you want to check
+:param y int: -> y you want to check
+:return bool: -> True if coordinates are in bounds, otherwise False
+        """
         if x < 0 or x+1 > self.width:   return False
         if y < 0 or y+1 > self.height:  return False
         return True
     
     def place_number(self, number, x, y):
-        if not self.in_bounds(x, y):    return False # Returns False if coordinates are not in bounds
-        if not number:                  return False # Returns False if number is 0
-        self._array[x,y] = number
+        """
+Places number in specified coords
+:param number int: -> Number you want to place
+:param x      int: -> x you want to place it in
+:param y      int: -> y you want to place it in
+:return bool: -> Success of the action; if there is already a number or coordinates are not in bounds it returns False
+        """
+        if not self.in_bounds(x, y):    return False
+        if not number:                  return False
+        self._array[x,y] = number # <- This can be kinda confuzing for new programmers, _array is actually a dictionary, not list. Its inserting number in key '(x,y)'
         return True
     
     def get_number(self, x, y):
+        """
+Checks what number is in specified coordinates
+:param x int: -> x you want to check
+:param y int: -> y you want to check
+:return int: -> Number thats on this coordinates, if there is not it returns 0
+        """
         try: return self._array[x,y]
         except KeyError: return 0
         
     def delete_number(self, x, y):
+        """
+Delete a number from coords
+:param x      int: -> x you want to delete
+:param y      int: -> y you want to delete
+:return bool: -> Success of the action; if there is't a number here, it returns False
+        """
         try:
             del self._array[x,y]
             return True
         except KeyError: return False
         
     def multiply_number(self, x, y):
+        """
+Multiplies number from coords
+:param x      int: -> x you want to multiply
+:param y      int: -> y you want to multiply
+:return bool: -> Success of the action
+        """
         number = self.get_number(x,y)*2
         return self.place_number(number,x,y)
     
     def _move_number(self, x, y, direction):
+        """
+Moves number from coordinates in specified direction
+:param x      int: -> x you want to move
+:param y      int: -> y you want to move
+:param direction enum Directions: -> direction you want to move the number in
+        """
         number = self.get_number(x, y)
         if not number: return # If target number is 0, we're done.
         
@@ -98,42 +182,59 @@ class Board:
         self.place_number(number, *target_position)
 
     def __move_numbers(self, direction):
+        """
+Private subtask method of move.
+:param direction enum Directions: -> Direction you want to move numbers in
+        """
         number_coords = tuple(self._array.keys())
         for coord in number_coords:
             self._move_number(*coord, direction)
             
     def move(self, direction, fill=True):
-        last_state = 0
-        current_state = 1
+        """
+Moving all numbers
+:param direction enum Directions: -> Direction you want to move numbers in
+:optional(True) param fill bool:  -> Execute random_fill() after moving?
+:raise AssertionError: -> If you think that html is a programming language and mess up with the code below
+        """
+        last_state    = "html"
+        current_state = "programming language"
+        # The two variables above have to be diffrent, otherwise we'll never enter the loop
+        assert last_state != current_state
         
-        while last_state != current_state:
+        while last_state != current_state: # While results of two moves in the row are different, it keeps trying till they're the same, so there is nothing else to move.
             last_state = tuple(self._array.keys())
             self.__move_numbers(direction)
             current_state = tuple(self._array.keys())
             
-        if fill: self.random_fill()
+        if fill: self.random_fill() # If fill is not False it fills the board
 
     def visualize(self, border=False, fixsize=True):
+        """
+Visualizes the board.
+:optional(False) param border  bool: -> Show border of the board?
+:optional(True)  param fixsize bool: -> Fixes size of the characters; in commandline two characters in the row makes a square
+:return str: -> The string of visualized board
+        """
         lines = []
         if border:
             headerlen = self.width
-            if fixsize:
-                headerlen *= 2
-            lines.append("+" + "-"*headerlen + "+")
+            if fixsize: headerlen *= 2
+            lines.append("+" + "-"*headerlen + "+") # Adds first line - the header
         
         for y in range(0, self.height):
             string = ""
-            if border: string += "|"
+            if border: string += "|" # Adds | as first character of the line
                 
             for x in range(0, self.width):
                 char = self._number_to_char(self.get_number(x,y))
                 if fixsize: char *= 2
                 string += char
 
-            if border: string += "|"
-            lines.append(string)
+            if border: string += "|" # Adds | as last character of the line
+            lines.append(string) # Append finished line to lines
     
         if border:
-            lines.append("+" + "-"*headerlen + "+")
+            lines.append("+" + "-"*headerlen + "+") # Adds last line - the footer
         return "\n".join(lines)
 
